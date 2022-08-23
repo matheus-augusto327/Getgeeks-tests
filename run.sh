50f6e4c8e657b96abe5ef8a157e1dc4302882d07
Matheus <matheus.augustoreal@gmail.com>
2022-08-23 08:18:00 -0300

    Tests in parallel with Pabot

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 robot -l NONE -o NONE -r NONE tasks/Delorean.robot
-robot -d ./logs -v BROWSER:chromuim -v HEADLESS:True tests/Signup.robot
+pabot -d ./logs -v BROWSER:chromuim -v HEADLESS:False tests
